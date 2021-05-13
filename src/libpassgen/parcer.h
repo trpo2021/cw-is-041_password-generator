07fff0b0317748be8976dac4f3e1b06152842e4e
NeveykoArt <neveiko29@gmail.com>
2021-05-14 00:03:25 +0700

    fix for codestyle

--- src/libpassgen/parcer.h
+++ src/libpassgen/parcer.h
@@ -1,7 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <getopt.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-bool getPasswordProperties(password_properties &property, int argc, char *argv[]);
+bool getPasswordProperties(
+        password_properties& property, int argc, char* argv[]);
