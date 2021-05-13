a4dffedb104498a1c6ff25cc4714a5f488533f6c
NeveykoArt <neveiko29@gmail.com>
2021-05-14 00:00:03 +0700

    make parcer

---
+++ src/libpassgen/parcer.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool getPasswordProperties(password_properties &property, int argc, char *argv[]);
