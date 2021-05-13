bc6e7b82ba1235c716867607796578281c1750fa
NeveykoArt <neveiko29@gmail.com>
2021-05-13 20:59:27 +0700

    add prototypes of check functions

---
+++ src/libpassgen/check.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+const char array_numbers[] = "0123456789";
+const char array_small_letter[] = "abcdefghijklmnopqrstuvwxyz";
+const char array_capital_letter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const char array_special_symbol[] = "!#$&\"'()*+,-./:;<=>?@[\\]^_`{|}~";
