80a08514468cdd8b1d94f7cbf5c49d4ca2e1e536
Gusev-Art <artemii.gusev@mail.ru>
2021-05-14 00:05:17 -0400

    write password generator

--- src/libpassgen/passgen.h
+++ src/libpassgen/passgen.h
@@ -1,1 +1,8 @@
 #pragma once
+#include "check.h"
+#include "helpInform.h"
+#include "parcer.h"
+#include "password_properties.h"
+
+char* generatorPassword(password_properties property);
+char** generatorPasswords(password_properties property);
