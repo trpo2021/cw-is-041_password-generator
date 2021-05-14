990937f1c8d69a35c1f2feaeca44949812311300
Gusev-Art <artemii.gusev@mail.ru>
2021-05-14 00:49:49 -0400

    write main

--- src/genpw/genpw.c
+++ src/genpw/genpw.c
@@ -1,8 +1,25 @@
 #include "libpassgen/passgen.h"
 #include <stdio.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-    printf("Hello, world\n");
+    srand(time(NULL));
+
+    password_properties property = initMediumPasswordProperties();
+
+    if (getPasswordProperties(&property, argc, argv)) {
+        char** passwords = generatorPasswords(property);
+
+        for (int i = 0; i < property.count; i++) {
+            printf("%s\n", passwords[i]);
+        }
+
+        for (int i = 0; i < property.count; i++) {
+            free(passwords[i]);
+        }
+
+        free(passwords);
+    }
+
     return 0;
 }
