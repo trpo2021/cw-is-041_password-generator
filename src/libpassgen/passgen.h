c2134e56fa2cf0f1711610f932dbb53403c024a3
Gusev-Art <artemii.gusev@mail.ru>
2021-05-14 00:50:48 -0400

    add header file

--- src/libpassgen/passgen.h
+++ src/libpassgen/passgen.h
@@ -3,6 +3,7 @@
 #include "helpInform.h"
 #include "parcer.h"
 #include "password_properties.h"
+#include <time.h>
 
 char* generatorPassword(password_properties property);
 char** generatorPasswords(password_properties property);
