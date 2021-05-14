fe1f5d0b45647ac831d5bcd33621aaf4d4399fcc
NeveykoArt <neveiko29@gmail.com>
2021-05-14 12:44:37 +0700

    codestyle

--- src/libpassgen/check.h
+++ src/libpassgen/check.h
@@ -1,9 +1,9 @@
 #pragma once
+#include "const.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "const.h"
 
 bool checkCount(int count);
 bool checkLength(int length);
