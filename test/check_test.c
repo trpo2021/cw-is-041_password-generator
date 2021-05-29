4c3438fbf1742e85233a82c07d93330b5d8b29d9
Gusev-Art <artemii.gusev@mail.ru>
2021-05-29 08:05:30 -0400

    group tests

--- test/check_test.c
+++ test/check_test.c
@@ -3,298 +3,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-CTEST(checkCount, count_test_1)
+CTEST(checkCount, count)
 {
-    int count = 10;
-
-    ASSERT_TRUE(checkCount(count));
+    ASSERT_TRUE(checkCount(10));
+    ASSERT_FALSE(checkCount(0));
+    ASSERT_FALSE(checkCount(50));
+    ASSERT_FALSE(checkCount(-10));
 }
 
-CTEST(checkCount, count_test_2)
+CTEST(checkLength, length)
 {
-    int count = 50;
-
-    ASSERT_FALSE(checkCount(count));
+    ASSERT_TRUE(checkLength(10));
+    ASSERT_FALSE(checkLength(1));
+    ASSERT_FALSE(checkLength(50));
+    ASSERT_FALSE(checkLength(-10));
 }
 
-CTEST(checkCount, count_test_3)
+CTEST(checkNumber, numbers)
 {
-    int count = 0;
-
-    ASSERT_FALSE(checkCount(count));
+    ASSERT_FALSE(checkNumber("a1", 1));
+    ASSERT_FALSE(checkNumber("aa", 2));
+    ASSERT_FALSE(checkNumber("1a", 0));
+    ASSERT_TRUE(checkNumber("123456", 6));
+    ASSERT_TRUE(checkNumber("1a", 2));
+    ASSERT_TRUE(checkNumber("12", 2));
+    ASSERT_TRUE(checkNumber("/.3456", 6));
+    ASSERT_TRUE(checkNumber("a1", 2));
 }
 
-CTEST(checkCount, count_test_4)
+CTEST(checkSmallLetter, small_letters)
 {
-    int count = -10;
-
-    ASSERT_FALSE(checkCount(count));
+    ASSERT_TRUE(checkSmallLetter("/23ab6", 6));
+    ASSERT_FALSE(checkSmallLetter("/23", 3));
+    ASSERT_FALSE(checkSmallLetter("SOS", 3));
+    ASSERT_TRUE(checkSmallLetter("sab", 3));
+    ASSERT_FALSE(checkSmallLetter("/2A", 3));
 }
 
-CTEST(checkLength, length_test_1)
+CTEST(checkCapitalLetter, cap_letters)
 {
-    int length = 10;
-
-    ASSERT_TRUE(checkLength(length));
+    ASSERT_TRUE(checkCapitalLetter("/2A", 3));
+    ASSERT_FALSE(checkCapitalLetter("/23", 3));
+    ASSERT_FALSE(checkCapitalLetter("abc", 3));
+    ASSERT_TRUE(checkCapitalLetter("SOS", 3));
 }
 
-CTEST(checkLength, length_test_2)
+CTEST(checkSpecialSymbol, spsymbol)
 {
-    int length = 1;
-
-    ASSERT_FALSE(checkLength(length));
-}
-
-CTEST(checkLength, length_test_3)
-{
-    int length = 50;
-
-    ASSERT_FALSE(checkLength(length));
-}
-
-CTEST(checkLength, length_test_4)
-{
-    int length = -10;
-
-    ASSERT_FALSE(checkLength(length));
-}
-
-CTEST(checkNumber, numbers_test_1)
-{
-    int size = 1;
-    char text[] = {'a', '1'};
-
-    ASSERT_FALSE(checkNumber(text, size));
-}
-
-CTEST(checkNumber, numbers_test_2)
-{
-    int size = 2;
-    char text[] = {'a', 'a'};
-
-    ASSERT_FALSE(checkNumber(text, size));
-}
-
-CTEST(checkNumber, numbers_test_3)
-{
-    int size = 0;
-    char text[] = {'1', 'a'};
-
-    ASSERT_FALSE(checkNumber(text, size));
-}
-
-CTEST(checkNumber, numbers_test_4)
-{
-    int size = 6;
-    char text[] = {
-            '1',
-            '2',
-            '3',
-            '4',
-            '5',
-            '6',
-    };
-
-    ASSERT_TRUE(checkNumber(text, size));
-}
-
-CTEST(checkNumber, numbers_test_5)
-{
-    int size = 2;
-    char text[] = {'1', 'a'};
-
-    ASSERT_TRUE(checkNumber(text, size));
-}
-
-CTEST(checkNumber, numbers_test_6)
-{
-    int size = 2;
-    char text[] = {'1', '2'};
-
-    ASSERT_TRUE(checkNumber(text, size));
-}
-
-CTEST(checkNumber, numbers_test_7)
-{
-    int size = 6;
-    char text[] = {
-            '/',
-            '.',
-            '3',
-            '4',
-            '5',
-            '6',
-    };
-
-    ASSERT_TRUE(checkNumber(text, size));
-}
-
-CTEST(checkNumber, numbers_test_8)
-{
-    int size = 2;
-    char text[] = {'a', '1'};
-
-    ASSERT_TRUE(checkNumber(text, size));
-}
-
-CTEST(checkSmallLetter, smallletters1)
-{
-    int size = 6;
-    char text[] = {
-            '/',
-            '2',
-            '3',
-            'a',
-            'b',
-            '6',
-    };
-
-    ASSERT_TRUE(checkSmallLetter(text, size));
-}
-
-CTEST(checkSmallLetter, small_letters_test_2)
-{
-    int size = 3;
-    char text[] = {
-            '/',
-            '2',
-            '3',
-    };
-
-    ASSERT_FALSE(checkSmallLetter(text, size));
-}
-
-CTEST(checkSmallLetter, small_letters_test_3)
-{
-    int size = 3;
-    char text[] = {
-            'S',
-            'O',
-            'S',
-    };
-
-    ASSERT_FALSE(checkSmallLetter(text, size));
-}
-
-CTEST(checkSmallLetter, small_letters_test_4)
-{
-    int size = 3;
-    char text[] = {
-            's',
-            'a',
-            'b',
-    };
-
-    ASSERT_TRUE(checkSmallLetter(text, size));
-}
-
-CTEST(checkSmallLetter, small_letters_test_5)
-{
-    int size = 3;
-    char text[] = {
-            '/',
-            '2',
-            'A',
-    };
-
-    ASSERT_FALSE(checkSmallLetter(text, size));
-}
-
-CTEST(checkCapitalLetter, cap_letters_test_1)
-{
-    int size = 3;
-    char text[] = {
-            '/',
-            '2',
-            'A',
-    };
-
-    ASSERT_TRUE(checkCapitalLetter(text, size));
-}
-
-CTEST(checkCapitalLetter, cap_letters_test_2)
-{
-    int size = 3;
-    char text[] = {
-            '/',
-            '2',
-            '3',
-    };
-
-    ASSERT_FALSE(checkCapitalLetter(text, size));
-}
-
-CTEST(checkCapitalLetter, cap_letters_test_3)
-{
-    int size = 3;
-    char text[] = {
-            'a',
-            'b',
-            'c',
-    };
-
-    ASSERT_FALSE(checkCapitalLetter(text, size));
-}
-
-CTEST(checkCapitalLetter, cap_letters_test_4)
-{
-    int size = 3;
-    char text[] = {
-            'S',
-            'O',
-            'S',
-    };
-
-    ASSERT_TRUE(checkCapitalLetter(text, size));
-}
-
-CTEST(checkSpecialSymbol, spsymbol_test_1)
-{
-    int size = 4;
-    char text[] = {
-            'T',
-            'R',
-            'P',
-            'O',
-    };
-
-    ASSERT_FALSE(checkSpecialSymbol(text, size));
-}
-
-CTEST(checkSpecialSymbol, spsymbol_test_2)
-{
-    int size = 4;
-    char text[] = {
-            '?',
-            '?',
-            '/',
-            '.',
-    };
-
-    ASSERT_TRUE(checkSpecialSymbol(text, size));
-}
-
-CTEST(checkSpecialSymbol, spsymbol_test_3)
-{
-    int size = 3;
-    char text[] = {
-            '6',
-            '6',
-            '6',
-    };
-
-    ASSERT_FALSE(checkSpecialSymbol(text, size));
-}
-
-CTEST(checkSpecialSymbol, spsymbol_test_4)
-{
-    int size = 3;
-    char text[] = {
-            'a',
-            'b',
-            '1',
-            'A',
-    };
-
-    ASSERT_FALSE(checkSpecialSymbol(text, size));
+    ASSERT_FALSE(checkSpecialSymbol("TRPO", 4));
+    ASSERT_TRUE(checkSpecialSymbol("\?\?/.", 4));
+    ASSERT_FALSE(checkSpecialSymbol("666", 3));
+    ASSERT_FALSE(checkSpecialSymbol("ab1A", 3));
 }
